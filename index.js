4995318a2554a4bf6f16cfa01f3a72ec67203492
Felipe <felipeameno2@gmail.com>
2020-09-15 21:07:12 -0300

    CSS

--- index.js
+++ index.js
@@ -18,11 +18,13 @@ function getRequest() {
 function cards(serie) {     //função para montar o CARD
     serie.forEach(element => { // Percorrendo todo o json para selecionar um elemento
         let card = `
-            <div>
-                <img src="${element.image_thumbnail_path}"></img>
-                <h2>${element.name}</h2>
-                <button onclick= "exibirDetalhes(${element.id})">CLIQUE AQUI</button>
-                
+            <div id="card" >
+                <div class="card">
+                 <h2>${element.name}</h2>
+                 <img src="${element.image_thumbnail_path}"></img>              
+                 <button onclick= "exibirDetalhes(${element.id})">CLIQUE AQUI</button>
+                </div>
+
             </div>
         `
         let teste2 = new DOMParser().parseFromString(card, 'text/html') //Manipulando o DOM, criando elementos dentro da pagina com JS.
@@ -32,7 +34,7 @@ function cards(serie) {     //função para montar o CARD
 
 }
 
-function exibirDetalhes(id) {
+function exibirDetalhes(id) {// função para exibir detalhes da serie 
     let requestUrl = `https://www.episodate.com/api/show-details?q=${id}`//Link da API 
     let request = new XMLHttpRequest(); //Função para requisiçaõ 
 
@@ -42,9 +44,22 @@ function exibirDetalhes(id) {
     request.onload = function () {
 
         let id = request.response;
-        console.log(id)
-
-
+        console.log(id);
+        modal(id);
     }
 
+}
+
+function modal(serie) {     //função para o modal
+    let card = `
+            <div class="popup">
+                <h2>${serie.tvShow.name}</h2>
+                 <img src="${serie.tvShow.image_thumbnail_path}"></img>
+            </div>
+        `
+    console.log("Oi me mama");
+
+    let teste2 = new DOMParser().parseFromString(card, 'text/html') //Manipulando o DOM, criando elementos dentro da pagina com JS.
+    teste2 = teste2.body.childNodes[0] //Rodando os elementos do meu array de objetos començando em 0.
+    let a = document.getElementById('modal').appendChild(teste2); // construindo no HTML os card para receber o nome.
 }
